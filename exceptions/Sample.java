8dda3a0165932139750838b6eae1648f420a78d5
Dmitry Nichiporenko <excelsiorious@gmail.com>
2017-07-07 03:18:48 +0300

    Main, Sample -> exceptions added

---
+++ exceptions/Sample.java
@@ -0,0 +1,22 @@
+package exceptions;
+
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+/**
+ * You will be given two integers x and y as input, you have to compute x/y . If and y are not 32 bit signed integers or if y is zero,
+ * exception will occur and you have to report it. Read sample Input/Output to know what to report in case of exceptions.
+ */
+public class Sample {
+    public static void getResult() {
+        Scanner scanner = new Scanner(System.in);
+        try {
+            int x = scanner.nextInt();
+            int y = scanner.nextInt();
+            System.out.println(x / y);
+        } catch (InputMismatchException | ArithmeticException e) {
+            System.out.println(e.toString());
+        }
+    }
+}
