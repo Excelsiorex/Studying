a8e8a81e7e8246c5843fa87673089b7adf818cb7
Dmitry Nichiporenko <excelsiorious@gmail.com>
2017-07-14 23:58:44 +0300

    Main, Factorial -> algorithms added

---
+++ algorithms/Main.java
@@ -0,0 +1,9 @@
+package algorithms;
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Factorial.getSimpleFactorial(3));
+        System.out.println(Factorial.getRecursiveFactorial(3));
+    }
+}
