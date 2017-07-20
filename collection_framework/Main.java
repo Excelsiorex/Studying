79c07e8c13156e2dca7db7bbbff410c9a0af01fa
Dmitry Nichiporenko <excelsiorious@gmail.com>
2017-07-20 16:17:52 +0300

    -

--- collection_framework/Main.java
+++ collection_framework/Main.java
@@ -3,8 +3,8 @@ package collection_framework;
 
 class Main {
     public static void main(String[] args) {
-        //Lists.getResult();
-        //Maps.getResult();
+        Lists.getResult();
+        Maps.getResult();
         Sets.getResult();
     }
 }
