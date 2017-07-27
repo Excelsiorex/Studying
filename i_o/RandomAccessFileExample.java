b2b5fa5ef2f9c1457a413659e6e6d48cc70490f5
Dmitry Nichiporenko <excelsiorit1@gmail.com>
2017-07-28 02:22:21 +0300

    Main, RandomAccessFileExample -> i_o updated

---
+++ i_o/RandomAccessFileExample.java
@@ -0,0 +1,35 @@
+package i_o;
+
+import java.io.FileNotFoundException;
+import java.io.RandomAccessFile;
+
+class RandomAccessFileExample {
+    static void randomAccess() {
+        try {
+            // Initialization
+            RandomAccessFile file = new RandomAccessFile("i_o/TextFile1.txt", "r");
+
+            // Full reading
+            System.out.println("FULL READING:");
+            byte[] bytes = new byte[(int) file.length()];
+            file.readFully(bytes);
+            for (byte b : bytes) {
+                System.out.print(((char) b));
+            }
+            System.out.println();
+
+            // Not full reading
+            System.out.println("NOT FULL READING:");
+            file.seek(4); // from
+            int c;
+            while ((c = file.read()) != -1) {
+                System.out.print(((char) c));
+            }
+
+            // End of working
+            file.close();
+        } catch (java.io.IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
