52410162fbf22c93a55a8ace58e0b9c08016a2cd
Dmitry Nichiporenko <excelsiorit1@gmail.com>
2017-08-20 20:03:46 +0300

    methods* -> parametrization updated

--- parametrization/methods/GenericMethods.java
+++ parametrization/methods/GenericMethods.java
@@ -1,7 +1,32 @@
 package parametrization.methods;
 
+import java.util.HashSet;
+import java.util.Set;
+
 class GenericMethods {
     <T> void f(T x) {
         System.out.println(x.getClass().getName());
+    }
+
+    <T> Set<T> union(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.addAll(b);
+        return result;
+    }
+
+    <T> Set<T> intersection(Set<T> a, Set<T> b) {
+        Set<T> result = new HashSet<>(a);
+        result.retainAll(b);
+        return result;
+    }
+
+    <T> Set<T> difference(Set<T> superset, Set<T> subset) {
+        Set<T> result = new HashSet<>(superset);
+        result.removeAll(subset);
+        return result;
+    }
+
+    <T> Set<T> complement(Set<T> a, Set<T> b) {
+        return difference(union(a, b), intersection(a, b));
     }
 }
--- parametrization/methods/Main.java
+++ parametrization/methods/Main.java
@@ -1,5 +1,9 @@
 package parametrization.methods;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
         GenericMethods gm = new GenericMethods();
@@ -9,5 +13,14 @@ public class Main {
         gm.f(100.0);
         gm.f(10L);
         gm.f(10.5F);
+
+        Set<Integer> integerSet = new HashSet<>(
+                Arrays.asList(1, 3, 5, 7, 9, 11, 13, 15));
+        Set<Integer> integerSet2 = new HashSet<>(
+                Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        System.out.println("Union: " + gm.union(integerSet, integerSet2));
+        System.out.println("Intersection: " + gm.intersection(integerSet, integerSet2));
+        System.out.println("Difference: " + gm.difference(integerSet, integerSet2));
+        System.out.println("Complement: " + gm.complement(integerSet, integerSet2));
     }
 }
